67fac2d060ec2c0aaddccb7c2067b5ff11e3ea20
Shahadat Hossain Shamim <shamim131666@gmail.com>
2024-07-08 19:41:55 +0600

    fix: navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import { Inria_Serif } from "next/font/google";
 import "./globals.css";
-import Navbar from "./components/Navbar";
 
 const inria_serif = Inria_Serif({ subsets: ["latin"], weight: "400" });
 
@@ -15,10 +14,7 @@ export default function RootLayout({ children }) {
       <head>
         <link rel="icon" href="/assets/quill_icon.svg" sizes="any" />
       </head>
-      <body className={inria_serif.className}>
-        <Navbar />
-        {children}
-      </body>
+      <body className={inria_serif.className}>{children}</body>
     </html>
   );
 }
