8d3a5c5da8f4a20af17f3bafc5e2290dab521826
Shahadat Hossain Shamim <shamim131666@gmail.com>
2024-09-01 14:11:39 +0600

    fixed: Steam Widget Size

--- src/app/components/Steam.js
+++ src/app/components/Steam.js
@@ -2,12 +2,16 @@
 const Steam = () => {
     return (
         <div className="w-full flex justify-center items-center mt-10">
-      <div>
-        <div className="flex justify-center">
-        <iframe src="https://store.steampowered.com/widget/3023460/" frameborder="0" width="646" height="190"></iframe>
+          <div className="w-full">
+            <div className="flex justify-center">
+              <iframe src="https://store.steampowered.com/widget/3023460/"
+              frameborder="0" 
+              className="w-full max-w-[300px] sm:max-w-[646px]"
+                        height="190"
+              ></iframe>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
     );
 };
 
